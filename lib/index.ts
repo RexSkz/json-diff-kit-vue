36ba07ee164f17b692ad1f8cfa904b739bb16253
Rex Zeng <rex@rexskz.info>
2024-04-29 01:18:14 +0800

    Initial commit

---
+++ lib/index.ts
@@ -0,0 +1,13 @@
+export { default as Differ } from 'json-diff-kit/differ';
+
+export type {
+  InlineDiffOptions,
+  InlineDiffResult,
+  ArrayDiffFunc,
+  DifferOptions,
+  DiffResult,
+} from 'json-diff-kit';
+
+export * from './viewer.vue';
+
+export type * from './types';
